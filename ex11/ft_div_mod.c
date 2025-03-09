1d45631ba539bcd5610194ec183c37509a914a11
KijijiKid <mateoandre@MacBook-Pro-von-Mateo.local>
2025-03-09 17:51:02 +0100

    ex11

---
+++ ex11/ft_div_mod.c
@@ -0,0 +1,15 @@
+void ft_div_mod(int a, int b, int *div, int *mod)
+{
+    *div = a / b;
+    *mod = a % b;
+}
+
+// int main()
+// {
+//     int a = 32;
+//     int b = 10;
+//     int c = 0;
+//     int d = 0;
+
+//     ft_div_mod(a, b, &c, &d);
+// }
